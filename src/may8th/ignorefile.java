7f113fe9a521a9198758522d5e05d9753845ead6
vtorushinap <64150775+vtorushinap@users.noreply.github.com>
2020-05-08 14:27:55 -0400

    Update ignorefile.java

--- src/may8th/ignorefile.java
+++ src/may8th/ignorefile.java
@@ -3,5 +3,8 @@ package may8th;
 public class ignorefile {
     public static void main(String[] args) {
         System.out.println("Hello beautiful world");
+        
+        //edited from github
+        
     }
 }
